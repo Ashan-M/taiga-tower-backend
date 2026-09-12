7ea95027b162d7d04548ca33068f318c1f9d7a8d
Ashan-M <ashanm@insharptechnologies.com>
2026-09-12 19:56:53 +0530

    Update DATABASE_URL environment variable name

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Set your PostgreSQL credentials in .env or update this string
-DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
